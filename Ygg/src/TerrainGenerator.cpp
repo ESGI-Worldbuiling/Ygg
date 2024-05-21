d8a1efda14ff7e81593d51c10802f19479c402de
Esilff <vbanari02@gmail.com>
2024-05-21 12:50:47 +0200

    [Dev] - Project setup

---
+++ Ygg/src/TerrainGenerator.cpp
@@ -0,0 +1,11 @@
+#include "TerrainGenerator.h"
+#include <tiny_gltf.h>
+
+namespace Ygg {
+    TerrainGenerator::TerrainGenerator() = default;
+
+    std::vector<float> TerrainGenerator::generate() {
+        tinygltf::Model m;
+
+    }
+}
---
+++ Ygg/src/TerrainGenerator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Ygg {
+    class TerrainGenerator {
+    public:
+        TerrainGenerator();
+
+        std::vector<float> generate();
+    private:
+
+
+    };
+}
